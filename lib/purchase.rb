bd9a46988778e98e24d39bd085c3a07b85c8a540
Luke Chinworth & Lee Jorgensen <student@epicodus.com>
2015-01-27 16:56:10 -0800

    start implementation of scope method for purchases between cates

--- lib/purchase.rb
+++ lib/purchase.rb
@@ -1,3 +1,7 @@
 class Purchase < ActiveRecord::Base
   has_and_belongs_to_many(:products)
+  scope(:between, -> do (start_and_end_dates)
+    where("created_at >= :start_date AND created_at <= :end_date", { start_date: start_date_and_end_dates.at(0), end_date: start_and_end_dates.at(1) })
+  end)
+
 end
